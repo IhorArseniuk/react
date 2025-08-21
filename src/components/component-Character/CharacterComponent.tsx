f1ff0318b3f9a745f88d4d787283f95883addfe1
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-08-21 21:06:27 +0200

    work at hw3

---
+++ src/components/component-Character/CharacterComponent.tsx
@@ -0,0 +1,11 @@
+import type {SimsonType} from "../../models/simsons.ts";
+import type {FC} from "react";
+
+export type CharacterType={
+    simson:SimsonType
+}
+export const CharacterComponent:FC<CharacterType> = (simson) => {
+    return (
+
+    );
+};
